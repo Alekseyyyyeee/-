49226cca218edb1ab169150fa641da32b68b79f4
Alekseyyyyeee <aleksey.vysockiy@list.ru>
2023-03-02 16:36:01 +0800

    Семинар знакомства с языками

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+string text = Console.ReadLine()!;
+int number;
+int.TryParse(text, out number);           //int number = int.Parse(text);
+
+
+int answer;
+answer = number * number;
+
+//Console.Write(number);
+//Console.Write(" ^ 2 = ");
+//Console.WriteLine(answer);
+Console.WriteLine($"{number} ^ 2 =  {answer}");  //Интерполяция: три строки вывода обработанной информации заменил одной
+Console.WriteLine("Всё работает.");
