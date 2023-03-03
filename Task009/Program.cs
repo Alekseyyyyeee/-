f7e367f364fb65c7808519a8aadbafe3ec4b80cd
Alekseyyyyeee <aleksey.vysockiy@list.ru>
2023-03-03 18:31:23 +0800

    Задача 9

---
+++ Task009/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Напишите программу, которая выводит случайное число из 
+отрезка {10, 99} и показывает наибольшую цифру числа. */
+
+int n = new Random().Next(10, 100);
+
+int a = n / 10;
+int b = n % 10;
+int  dig = a;
+if (dig < b) dig = b;
+Console.WriteLine(n);
+Console.WriteLine(dig);
