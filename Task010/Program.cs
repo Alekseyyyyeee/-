2920fc191ea0a71512ee5cd1e88759e3b622b0b6
Alekseyyyyeee <aleksey.vysockiy@list.ru>
2023-03-04 18:44:38 +0800

    Задача 10

---
+++ Task010/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход 
+трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+
+int num;
+Console.Write($"Введите трёхзначное число: ");
+int.TryParse(Console.ReadLine()!, out num);
+
+int result;
+int num2;
+
+num2 = num / 10;
+result = num2 % 10;
+
+Console.WriteLine(result);
