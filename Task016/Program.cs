6a6c52c33c5e6587319563bfbbc012109695c397
Alekseyyyyeee <aleksey.vysockiy@list.ru>
2023-03-04 17:45:04 +0800

    Задача 16 2

--- Task016/Program.cs
+++ Task016/Program.cs
@@ -7,7 +7,7 @@ int.TryParse(Console.ReadLine()!, out num1);
 Console.Write($"Введите второе число: ");
 int.TryParse(Console.ReadLine()!, out num2);
 
-if (num1 == num2 * num2)
+if (num1 == num2 * num2 || num2 == num1 * num1)
     Console.Write("Да");
 else
     Console.WriteLine("Нет");
