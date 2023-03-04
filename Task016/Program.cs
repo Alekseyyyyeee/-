59a40ceddfbe06db409425f300c105b383f5090f
Alekseyyyyeee <aleksey.vysockiy@list.ru>
2023-03-04 17:39:28 +0800

    Задача 16

---
+++ Task016/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая принимает на вход два 
+числа и проверияет, является ли первое число квадратом вторго. */
+
+int num1, num2;
+Console.Write($"Введите первое число: ");
+int.TryParse(Console.ReadLine()!, out num1);
+Console.Write($"Введите второе число: ");
+int.TryParse(Console.ReadLine()!, out num2);
+
+if (num1 == num2 * num2)
+    Console.Write("Да");
+else
+    Console.WriteLine("Нет");
+
