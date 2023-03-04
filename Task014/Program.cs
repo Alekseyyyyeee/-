3e6de742b300717b4b1423ad15840413eb5aadb4
Alekseyyyyeee <aleksey.vysockiy@list.ru>
2023-03-04 10:10:06 +0800

    Задача 13

---
+++ Task014/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Напишите программу, которая принимает на вход число 
+ и проверяет, кратно ли оно одновременно 7 и 23. */
+
+int num1;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out num1);
+
+if (num1 % 7 == 0 && num1 % 23 == 0)
+    Console.WriteLine("Да");
+else
+    Console.WriteLine("Нет");
