8178d45cddd385221e58205322ae4bc1f8b7dfe4
Alekseyyyyeee <aleksey.vysockiy@list.ru>
2023-03-07 20:24:10 +0800

    Задача 18

---
+++ Task018/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Напишите программу, которая по заданному номеру четверти,
+показывает диапазон возможных координат точек в этой четверти. */
+
+int part;
+Console.Write("Введите плоскость: ");
+int.TryParse(Console.ReadLine()!, out part);
+
+switch (part)
+{
+    case 1:
+        Console.WriteLine("X > 0, Y > 0");
+        break;
+    case 2:
+        Console.WriteLine("X < 0, Y > 0");
+        break;
+    case 3:
+        Console.WriteLine("X < 0, Y < 0");
+        break;
+    case 4:
+        Console.WriteLine("X > 0, Y < 0");
+        break;
+    default:
+        Console.WriteLine("Неверный ввод");
+        break;
+}
+
+/* if (part == 1)
+    Console.WriteLine("X > 0, Y > 0");
+else if (part == 2)
+    Console.WriteLine("X < 0, Y > 0");
+else if (part == 3)
+    Console.WriteLine("X < 0, Y < 0");
+else if (part == 4)
+    Console.WriteLine("X > 0, Y < 0");
+else
+    Console.WriteLine("Неверный ввод"); */
