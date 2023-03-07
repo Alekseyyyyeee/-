1a56881f0650573c724988d97157cf2a5c4d37b2
Alekseyyyyeee <aleksey.vysockiy@list.ru>
2023-03-08 03:07:25 +0800

    Задача 19 3

--- Task019/Program.cs
+++ Task019/Program.cs
@@ -14,6 +14,6 @@ double.TryParse(Console.ReadLine()!, out B[1]);
                                                  //                                     2 - сама степень
 double AC2 = (A[0] - B[0]) * (A[0] - B[0]);      // double AC2 = Math.Pow((A[0] - B[0]) 2);  - то же самое, но читать проще
 double BC2 = (A[1] - B[1]) * (A[1] - B[1]);      //              Math.Pow - возведение в степень
-double AB = Math.Sqrt(AC2 + BC2);
+double AB = Math.Sqrt(AC2 + BC2);                // Math.Sqrt -квадратный корень
 
 Console.Write($"Расстояние между точками A({A[0]}; {A[1]}) и B({B[0]}; {B[1]}) = {AB}");
