142458390e13fdb44a6770ece4fa6b295a635676
Alekseyyyyeee <aleksey.vysockiy@list.ru>
2023-03-08 16:10:35 +0800

    Палиндромы

--- Task019/Program.cs
+++ Task019/Program.cs
@@ -1,19 +1,31 @@
-﻿/* Напишите программу, которая принимает на вход координаты
-двух точек и находит растояние между ними в 2D пространстве. */
+﻿/* 19Напишите программу, которая принимает на вход пятизначное число и 
+проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
 
-double[] A = new double[2];
-double[] B = new double[2];
-Console.Write("Введите координату X точки A: ");
-double.TryParse(Console.ReadLine()!, out A[0]);
-Console.Write("Введиет координату Y точки A: ");
-double.TryParse(Console.ReadLine()!, out A[1]);
-Console.Write("Введите координату X точки B: ");
-double.TryParse(Console.ReadLine()!, out B[0]);
-Console.Write("Введите координату Y точки B: ");
-double.TryParse(Console.ReadLine()!, out B[1]);
-                                                 //                                     2 - сама степень
-double AC2 = (A[0] - B[0]) * (A[0] - B[0]);      // double AC2 = Math.Pow((A[0] - B[0]) 2);  - то же самое, но читать проще
-double BC2 = (A[1] - B[1]) * (A[1] - B[1]);      //              Math.Pow - возведение в степень
-double AB = Math.Sqrt(AC2 + BC2);                // Math.Sqrt -квадратный корень
+int num;
+Console.Write("Введите пятизначное число: ");
+int.TryParse(Console.ReadLine()!, out num);
+if (num < 1000 || num > 100000)
+{
+    Console.WriteLine("Некорректное число");
+}
+else if ((num / 10000) == (num % 10))
+    if (((num / 1000) % 10) == ((num % 100) / 10))
+    {
+        Console.WriteLine($"{num} -> да");
+    }
+    else
+    {
+        Console.WriteLine($"{num} -> нет");
+    }
+else if ((num / 10000) != (num % 10))
+{
+    Console.WriteLine($"{num} -> нет");
+}
+/*Console.WriteLine($"{num / 10000}" );
+Console.WriteLine($"{num % 10}" );
 
-Console.Write($"Расстояние между точками A({A[0]}; {A[1]}) и B({B[0]}; {B[1]}) = {AB}");
+Console.WriteLine($"{(num / 1000) % 10}" );
+Console.WriteLine($"{(num % 100) / 10}" );*/
