c4610678fe34c8c048ed991b334af87f4ad25ca9
Alekseyyyyeee <aleksey.vysockiy@list.ru>
2023-03-08 13:07:40 +0800

    Задача 20

---
+++ Task020/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая принимает на вход число (N)
+и выдаёт таблицу квадратов чисел от одного до N. */
+
+int N;
+Console.Write("ВВедите N: ");
+int.TryParse(Console.ReadLine()!,out N);
+
+int i = 1; int result;
+while (i < N)
+{
+    result = i * i;
+    Console.Write($"{result}, ");
+    i++;
+}
+result = N * N;
+Console.Write($"{result} ");
