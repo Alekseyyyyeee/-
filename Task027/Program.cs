46d8b3ba8fa442486f8c13fe15667012ad4d063a
Alekseyyyyeee <aleksey.vysockiy@list.ru>
2023-03-13 20:42:30 +0800

    Задача 27

---
+++ Task027/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 27: Напишите программу, которая принимает
+на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+
+int num;
+Console.Write("Введите число: ");
+int.TryParse(Console.ReadLine()!, out num);
+int sum = 0;
+while (num > 0)
+{
+    sum = sum + num % 10;
+    num = num / 10;
+}
+Console.WriteLine($"{sum}");
