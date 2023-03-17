329afda53c3b7fd35df37d02a7bab474e5e7fbbd
Alekseyyyyeee <aleksey.vysockiy@list.ru>
2023-03-18 01:14:06 +0800

    З44ача 42

---
+++ Task044/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 44   Не используя рекурсиюБ выведите первые N чисел Фибоначчи.
+Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8 */
+
+
+
+int num;
+Console.WriteLine("Введите число ");
+int.TryParse(Console.ReadLine()!, out num);
+
+
+int left = 0; int right = 1;
+
+Console.Write($"{left} {right} ");
+
+for (int i = 2; i < num; i++)
+{
+    right = left + right;
+    left = right - left;
+    Console.Write($"{right} ");
+}
