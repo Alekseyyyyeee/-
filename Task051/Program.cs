18eb1f643167a246b5feff70ed95beab3f65cdcc
Alekseyyyyeee <aleksey.vysockiy@list.ru>
2023-03-19 23:44:27 +0800

    Задача 51

---
+++ Task051/Program.cs
@@ -0,0 +1,29 @@
+﻿/*51 Задайте двумерный массив. Найдите сумму элементов,
+находящихся на главной диагонали.*/
+
+
+int m = 5; int n = 5;
+int[,] massive = new int[m, n];
+
+for (int i = 0; i < massive.GetLength(0); i++)
+{
+    for (int j = 0; j < massive.GetLength(1); j++)
+    {
+        massive[i, j] = new Random().Next(10);
+        Console.Write($"{massive[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+int sum = 0;
+for (int i = 0; i < massive.GetLength(0); i++)
+{
+    
+    for (int j = 0; j < massive.GetLength(1); j++)
+    {
+       
+        if (i == j)
+            sum = sum + massive[i, j];
+    }
+}
+Console.WriteLine($"{sum}");
