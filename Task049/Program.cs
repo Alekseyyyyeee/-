10a46e227b18a4b331e13b27a92f279b6d22dd36
Alekseyyyyeee <aleksey.vysockiy@list.ru>
2023-03-19 23:04:33 +0800

    Задача 49

---
+++ Task049/Program.cs
@@ -0,0 +1,37 @@
+﻿/*49 Задайте двумерный массив. Найдите элементы, у которых 
+оба индекса чётные, и замените эти элементы на их квадраты.*/
+
+int m = 10; int n = 30;
+
+int[,] massive = new int[m, n];
+
+for (int i = 0; i < massive.GetLength(0); i++)
+{
+    for (int j = 0; j < massive.GetLength(1); j++)
+    {
+        massive[i, j] = new Random().Next(10);
+        Console.Write($"{massive[i, j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+
+for (int i = 2; i < massive.GetLength(0); i = i + 2)
+{
+    for (int j = 2; j < massive.GetLength(1); j = j + 2)
+    {
+            massive[i, j] = massive[i, j] * massive[i, j];
+    }
+
+}
+
+
+
+
+for (int i = 0; i < massive.GetLength(0); i++)
+{
+    for (int j = 0; j < massive.GetLength(1); j++)
+        Console.Write($"{massive[i, j]} ");
+    Console.WriteLine();
+}
