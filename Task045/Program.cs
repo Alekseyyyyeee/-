128b4f04302c4791f0827df19917bdcd7bccbeb6
Alekseyyyyeee <aleksey.vysockiy@list.ru>
2023-03-19 22:50:27 +0800

    Задача 48

---
+++ Task045/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Двумерные массивы. Всё работает.
+
+
+int[,] array = new int[2, 10];
+
+for (int i = 0; i < array.GetLength(0); i++)   //Строка
+{
+    for (int j = 0; j < array.GetLength(1); j++ )    //Элементы строки (столбцы)
+    {
+        array[i, j] = new Random().Next(100);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}*/
+
+/*Задача 48. Задайте двумерный массив размера m на n. 
+Каждый элемент массива находится по формуле Amn = m + n. 
+Выведите полученный массив на экран.
+m = 3
+n = 4 */
+
+int m = 10; int n = 18;
+int[,] massive = new int[m, n];
+
+for (int i = 0; i < massive.GetLength(0); i++)
+{
+    for (int j = 0; j < massive.GetLength(1); j++)
+    {
+        massive[i, j] = i * j;  //Тут я умножил - мне так интереснее
+        Console.Write($"{massive[i, j]} ");
+    }
+    Console.WriteLine();
+}
