18e4fa5a10610017396853d80d75f45f121ee253
Alekseyyyyeee <aleksey.vysockiy@list.ru>
2023-03-25 20:08:43 +0800

    Задача 53 решение через list

--- Task053/Program.cs
+++ Task053/Program.cs
@@ -2,7 +2,7 @@
 поменяет местами первую и последнюю строки массива.*/
 
 
-int m = 3; int n = 4;
+/*int m = 3; int n = 4;
 int[,] array = new int[m, n]; 
 for (int i = 0; i < array.GetLength(0); i++)
 {
@@ -29,6 +29,35 @@ for (int i = 0; i < array.GetLength(0); i++)
     for (int j = 0; j < array.GetLength(1); j++)
     {
         Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}*/
+
+
+List<List<int>> array = new List<List<int>>();
+
+for (int i = 0; i < 3; i++)
+{
+    array.Add(new List<int>());
+    for (int j = 0; j < 4; j++)
+    {
+        array[i].Add(new Random().Next(10));
+        Console.Write($"{array[i][j]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+int lastrow = array.Count - 1;
+(array[0], array[lastrow]) = (array[lastrow], array[0]);
+
+for (int i = 0; i < 3; i++)
+{
+    array.Add(new List<int>());
+    for (int j = 0; j < 4; j++)
+    {
+        Console.Write($"{array[i][j]} ");
     }
     Console.WriteLine();
 }
