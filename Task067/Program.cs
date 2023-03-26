21a92d0551a886bb222af9935b4ee0de7188cb8f
Alekseyyyyeee <aleksey.vysockiy@list.ru>
2023-03-27 00:28:55 +0800

    З67ача 56

---
+++ Task067/Program.cs
@@ -0,0 +1,18 @@
+﻿/*67 Напишите программу, которая будет принимать 
+на вход число, и возвращать сумму его цифр*/
+
+
+int n;
+
+Console.Write("Введите число: ");
+int.TryParse(Console.ReadLine()!, out n);
+
+
+Console.Write(Fact(n));
+
+int Fact(int n)
+{
+    if (n < 10)
+        return n;
+    return n % 10 + Fact(n / 10);
+}
