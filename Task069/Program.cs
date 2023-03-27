5bcf6e6c3194a052a40b029b7872099e6cfca5f7
Alekseyyyyeee <aleksey.vysockiy@list.ru>
2023-03-28 02:24:20 +0800

    Задача 69

---
+++ Task069/Program.cs
@@ -0,0 +1,19 @@
+﻿/*69 Напишите прорамму, которая на вход принимает два числа A и B,
+и вазводит число A в сцелую степень B с помощью рекурсии*/
+
+
+int a; int b;
+
+Console.Write("Введите число A: ");
+int.TryParse(Console.ReadLine()!, out a);
+Console.Write("Введите число B: ");
+int.TryParse(Console.ReadLine()!, out b);
+
+Console.Write(Pow(a, b));
+
+int Pow(int a, int b)
+{
+    if (b == 0)
+        return 1;
+    return a * Pow(a, b - 1);
+}
